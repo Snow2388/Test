073684a37da8fefb662338d48ddb8856651637f4
Snow2388 <151289225+Snow2388@users.noreply.github.com>
2024-03-06 13:20:56 +0800

    111

---
+++ 测试/测试/字符转换.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void chgStr(char* ip, char* op)
+{
+
+}
+
+
+int main()
+{
+	char* ip = new char[10];
+	char* op = new char[10];
+	cin >> ip;
+
+	for (; *ip != '\0'; ip++)
+
+		cout << *ip;
+
+}
